6370d3ec60d77bcf88c48c13967aa67bc3a5a30b
qooktree1 <qooktree@gmail.com>
2022-05-20 10:15:35 +0900

    0520_이제 시작이야 내꿈을

--- final-pjt-back/movies/urls.py
+++ final-pjt-back/movies/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'movies'
 urlpatterns = [
     path('', views.index),
+    path('detail/<int:movie_pk>', views.movie_detail),
 ]
--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -10,7 +10,7 @@ from .serializers import MovieSerializer
 
 @api_view(['GET'])
 def index(request):    
-    highscore_movies = Movie.objects.order_by('-vote_average')
+    highscore_movies = Movie.objects.order_by('-vote_average').prefetch_related('genres')
     hightscore_serializer = MovieSerializer(data=highscore_movies, many=True)
     hightscore_serializer.is_valid()  # 왜 이렇게 사용할까??
     context = {
@@ -20,5 +20,12 @@ def index(request):
 
 
 @api_view(['GET'])
-def movie_detail(request):
-    pass
+def movie_detail(request, movie_pk):
+    movie = Movie.objects.get(pk=movie_pk)
+    print(movie)
+    # 여기서 빨래 색깔 관련 serialized data, 빨래 시간 관련 data 를 context에 삽입??!
+
+    context = {
+
+    }
+    return Response(context)
