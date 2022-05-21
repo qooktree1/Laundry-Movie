0517c88a7538c4c3ec5f9ec0d094bd1e5514f66b
qooktree1 <qooktree@gmail.com>
2022-05-21 15:16:32 +0900

    영화추천페이지 작성중

--- Laundry-Movie/movies/urls.py
+++ Laundry-Movie/movies/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('home/', views.home, name='home'),
     path('tips/', views.tips, name='tips'),
+    path('selected/', views.selected, name='selected'),
     
     path('allmovie/', views.movie_list, name='movie_list'),
     path('detail/<int:movie_pk>', views.movie_detail, name='movie_detail'),
