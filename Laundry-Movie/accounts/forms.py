93c877776d7109743a55e5410ec22e586f8a72cd
Binzify <beingbright95@gmail.com>
2022-05-26 11:30:05 +0900

    220526_search_nav_change

--- Laundry-Movie/accounts/forms.py
+++ Laundry-Movie/accounts/forms.py
@@ -3,7 +3,6 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
 from django.contrib import messages
 from .models import Profile, User
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.db import models
 
 
@@ -20,7 +19,7 @@ class LoginForm(forms.ModelForm):
         fields = ('username', 'password',)
 
 
-# 
+
 class ProfileForm(forms.ModelForm):
     image = forms.ImageField(required=False)
    	# 위의 내용을 정의하지 않아도 상관없지만, 화면에 출력될 때 label이 영문으로 출력되는 것이 싫어서 수정한 것이다..
